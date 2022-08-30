7850eaffabf886f637a9d86c3e4e8580b4f272f6
Jesus <juniorpastorini_@hotmail.com>
2022-08-30 17:49:32 -0300

    paginas

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,19 @@
+import Input from '../form/Input'
+import styles from './ProjectForm.module.css'
+function ProjectForm(){
+    return(
+        <form className={styles.form}>
+           <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto" />
+           <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento total" />
+            <div>
+                <select name="category_id">
+                    <option disabled selected>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Criar Projeto" />
+            </div>
+        </form>
+    )
+}
+export default ProjectForm
